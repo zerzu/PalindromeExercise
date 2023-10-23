ca9baf5d2b2e5c23c356031059000a9411cd9ee1
Eric Garner <ericgarner81@gmail.com>
2023-10-23 13:03:09 -0500

    completed exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            string reverse = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+               reverse += word.Substring(i, 1);
+            }
+
+            return word.CompareTo(reverse) == 0;
+
+
+        }
+
+        
+
+    }
+
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,31 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("dog", false)]
+        [InlineData("cat", false)]
+        [InlineData("civic", true)]
+        [InlineData("noon", true)]
+        [InlineData("tattarrattat", true)]
 
+        public void Test1(string name, bool expected)
+        {
+            //Arrange
+            var pal = new WordSmith();
+
+            //Act
+
+            var actual = pal.IsAPalindrome(name);
+
+            //Assert
+
+            Assert.Equal(expected, actual);
         }
     }
 }
